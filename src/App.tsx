506c81c2197e2c3396c6db3282983aa7a15cd708
hoshinaoshi <hoshinaoshi@gmail.com>
2018-12-09 14:13:40 +0900

    tslintの設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default () => (
-  <View style={{ marginTop: 50 }}>
-    <Text>Hello Expo!</Text>
-  </View>
-);
+export default class App extends React.Component {
+  public render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
